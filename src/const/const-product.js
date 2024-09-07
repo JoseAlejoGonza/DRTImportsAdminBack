f539bc728e8eac0819e3f7262f33f3eb3c1d1f5c
JoseAlejoGonza <jose_alejogonza@hotmail.com>
2024-09-07 12:59:45 -0500

    functionality to search and edit product is added

--- src/const/const-product.js
+++ src/const/const-product.js
@@ -1,11 +1,15 @@
 const PRODUCT_EXIST = 'product_already_exists';
 const INSERT_SUCCESFUL = 'insert_successful';
+const UPDATE_SUCCESFUL = 'update_successful';
 const INSERT_FAILED = 'insert_failed';
+const UPDATE_FAILED = 'update_failed';
 const ERROR = 'not_found';
 
 module.exports = {
     PRODUCT_EXIST,
     INSERT_SUCCESFUL,
+    UPDATE_SUCCESFUL,
     INSERT_FAILED,
+    UPDATE_FAILED,
     ERROR
 }
